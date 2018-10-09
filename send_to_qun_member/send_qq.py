4c4aaaa45ccb7fccc22219655a2267c52de6f3ff
三个和尚 <ahaufox@vip.qq.com>
2018-10-09 11:53:30 +0800

    更新

--- send_to_qun_member/send_qq.py
+++ send_to_qun_member/send_qq.py
@@ -525,12 +525,12 @@ def send_qq(qq_id,times):
 #check_cookies(547575116)
 #update_qun_and_qq(547575116)
 #insert_all_qq(547575116)
-send_qq(452193182, 5000)
+#send_qq(452193182, 5000)
 
-# for i in range(0,10):
-#     try:
-#         send_qq(452193182,5000)
-#     except:
-#         print ("err")
-#     else:
-#         send_qq(452193182, 5000)
+for i in range(0,10):
+    try:
+        send_qq(452193182,5000)
+    except:
+        print ("err")
+    else:
+        send_qq(452193182, 5000)
